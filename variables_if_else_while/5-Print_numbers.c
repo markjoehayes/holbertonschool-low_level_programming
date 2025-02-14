bbe7f6834821b5103977be43bf573eaa5ce871a6
markjoehayes <markjoehayes@proton.me>
2025-02-14 09:18:05 -0400

    more looping

---
+++ variables_if_else_while/5-Print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ * Return: zero is success
+ */
+
+ int main(void)
+ {
+	 int i;
+
+	 for (i = 0; i < 10; i++)
+	 {
+		 printf("%d", i);
+	 }
+	 puts("\n");
+
+ }
---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ * Return: zero is success
+ */
+
+ int main(void)
+ {
+	 int i;
+
+	 for (i = 0; i < 10; i++)
+	 {
+		 printf("%d", i);
+	 }
+	 puts("\n");
+	 return (0);
+
+ }
