a8f1d7e732d095e4c21fcadfeb8d21ccd81eed7a
markjoehayes <markjoehayes@proton.me>
2025-02-14 11:22:42 -0400

    looping and breaj

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ * Return: zero is success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+
+
+}
