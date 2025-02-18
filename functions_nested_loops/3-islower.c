d461d4d18fe9f4d9a5643d11726eff4d10802bb8
markjoehayes <markjoehayes@proton.me>
2025-02-18 11:25:19 -0400

    final betty style, for real

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,11 +4,11 @@
  * _islower - checks for lower case character
  */
 
-int _islower (int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	if (c >= 'A' && c <= 'Z')
 		return (0);
-	return(0);
+	return (0);
 }
