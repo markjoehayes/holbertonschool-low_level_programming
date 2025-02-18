b2e00a096d3d7092e8c8ea9c0130fa23c455b7bc
markjoehayes <markjoehayes@proton.me>
2025-02-18 11:30:27 -0400

    betty doc fix

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lower case character
  *@c: The letter to be tested
- * Return : 1 for lower and 0 for upper
+ * Return: 1 for lower and 0 for upper
  */
 
 int _islower(int c)
