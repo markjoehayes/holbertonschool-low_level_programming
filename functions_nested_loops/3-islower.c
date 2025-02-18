f7b9f73b2d7bce9d2b99f0b48cc562158c3eb68c
markjoehayes <markjoehayes@proton.me>
2025-02-18 11:17:31 -0400

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case character
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (0);
+	return(0);	
+}
