8da527246c923ced362913be99a323867237d93a
markjoehayes <markjoehayes@proton.me>
2025-02-20 09:25:03 -0400

    betty style and betty doc

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 
+/**
+ * main - calculate sum of multiples 0f 3 and 5 below 1024
+ * Return: zero is success!
+ */
+
 int main(void)
 {
 	int i;
 	int sum = 0;
 
-	for (i = 0; i <= 1024; i ++)
+	for (i = 0; i <= 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
