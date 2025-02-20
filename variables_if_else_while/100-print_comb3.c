441b546114fba03e77256a7269088595216f5d95
markjoehayes <markjoehayes@proton.me>
2025-02-20 13:25:48 -0400

    betty style fix

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (; num1 <= '9'; num1++)
 	{
-		for(num2 = num1 + 1; num2 <= '9'; num2++)
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
 		{
 			putchar(num1);
 			putchar(num2);
