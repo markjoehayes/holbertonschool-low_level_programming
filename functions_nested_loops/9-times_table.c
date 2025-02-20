6a865d9cba7bef1fd7a9d99edc394fb8c2333de4
markjoehayes <markjoehayes@proton.me>
2025-02-20 09:20:29 -0400

    total sum

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,19 +8,5 @@
 
 void times_table(void)
 {
-	int a, b;
-
-	for (a = 0; a <= 9; a++)
-	{
-		_putchar('0' + '0');
-		_putchar(',' + '0');
-		_putchar(32 + '0');
-		for (b = 0; b <= 9; b++)
-		{
-			_putchar(b + '0');
-		}
-		_putchar('\n');
-		_putchar(a + '0');
-	}
 	
 }
