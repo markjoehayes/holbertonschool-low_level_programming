aa8feb36ee79c4648fd37b5669b3d7c03209d221
markjoehayes <markjoehayes@proton.me>
2025-02-24 09:55:08 -0400

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer to be tested
+ * @b: integer to be tested
+ * Return: return product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
