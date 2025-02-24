75338db4bffd4ea71dbbfdf13f372a28aa570d40
markjoehayes <markjoehayes@proton.me>
2025-02-24 14:08:33 -0400

    square

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagnol line in stdout 
+ * @n: length of line 
+ */
+
+void print_diagonal(int n)
+{
+	if (n >0)
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+
+		}
+	}
+}
+
---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of hash based on size 
+ * @size: size of square
+ */
+
+void print_square(int size) 
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
+
