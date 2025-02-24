db4b36938cf736e07979bbeabc939550a8c53c8e
markjoehayes <markjoehayes@proton.me>
2025-02-24 09:33:01 -0400

    is-upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper: checks for uppercase character
+ * @ch: character be tested
+ * Return: 1 for uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+	return (0);
+}
+
---
+++ more_functions_nested_loops/template.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - 
+ * @n: integer to be tested
+ * Return: 
+ */
+
+int _abs(int n)
+{
+
+}
+
