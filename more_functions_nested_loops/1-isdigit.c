7894461ae267f851792d9b722901eb5305ffb198
markjoehayes <markjoehayes@proton.me>
2025-02-24 09:46:49 -0400

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if digit 
+ * @c: character to be tested 
+ * Return: 1 for digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+	return (0);
+}
+
