d4db2c392c1ad03fa05529b58161ab147ede6fdf
markjoehayes <markjoehayes@proton.me>
2025-02-25 09:42:12 -0400

    correct last line space

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,17 +4,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
-		printf("%d", i);
-
 		if (i % 3 == 0 && i % 5 == 0)
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf(" Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf(" Buzz");
+			printf("Buzz ");
+		else
+			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
