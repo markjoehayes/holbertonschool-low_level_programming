603e78a520352ab0d2acf88906768ada099d24c9
markjoehayes <markjoehayes@proton.me>
2025-02-26 13:12:52 -0400

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -9,9 +9,11 @@ void _puts(char *str)
 {
 	int counter = 0;
 
-	while (*str != '\0')
+	while (counter >= 0)
 	{
 		_putchar(str[counter]);
+		if (str[counter] == '\0')
+			break;
 		counter++;
 	}
 	_putchar('\n');
