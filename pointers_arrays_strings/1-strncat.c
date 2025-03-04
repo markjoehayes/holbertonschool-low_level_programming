daa03235f87335313c9556e55dc730a98da403dd
markjoehayes <markjoehayes@proton.me>
2025-03-04 09:46:10 -0400

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - this fuction will concatenates two strings
+ * @dest: the string that will be modified.
+ * @src: the string that will be concatenated to @dest.
+ * @n: copy up to poistion 'n'
+ * Return: a pointer to @dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
+
