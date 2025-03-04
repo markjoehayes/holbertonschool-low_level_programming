358ba7f3c9ca303e46f2f30247de67a57b0f958e
markjoehayes <markjoehayes@proton.me>
2025-03-04 04:29:40 -0400

    reverse

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,16 @@
 
 void rev_string(char *s)
 {
-	int counter;
+	int len = 0, index = 0;
+	char tmp;
 
-	for (counter = 0; ; counter++)
-	{
-		if (s[counter] == '\0')
-			break;
-	}
-	for (counter -= 1; counter >= 0; counter--)
-	{
-		s[counter] = counter;
-	}
+	while (s[index++])
+		len++;
 
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+	}
 }
-
