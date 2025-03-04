0d6586aedef01f084fa5df951c6a6e1fcf3666b2
markjoehayes <markjoehayes@proton.me>
2025-03-04 11:27:52 -0400

    fix space issues for betty

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -9,8 +9,8 @@
  * Return: pointer to memory area s
  */
 
- char *_memset(char *s, char b, unsigned int n)
- {
+char *_memset(char *s, char b, unsigned int n)
+{
 	unsigned int i = 0;
 	char *start = s;
 
