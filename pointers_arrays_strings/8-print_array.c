22131852d60ac124e0563a1eef0bb7e872b9521f
markjoehayes <markjoehayes@proton.me>
2025-03-06 14:13:21 -0400

    chessboard

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,18 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+	while (i < n)
 	{
-		if (i < (n-1))
+		if (i != n - 1)
 			printf("%d, ", a[i]);
-		if (i == (n - 1))
-			printf("%d\n", a[i]);
+		else
+			printf("%d", a[i]);
+		i++;
 	}
-
+	putchar('\n');
 }
+
+
+
 
---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of two diagonals of a square matrix of integers
+ * @a: 
+ * @size: size of array
+ */
+
+ void print_diagsums(int *a, int size)
+ {
+
+ }
