b0d941a387deec140eb44281d8433947d4bb6440
markjoehayes <markjoehayes@proton.me>
2025-03-06 10:30:59 -0400

    style fix3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,12 +6,12 @@
  * Return: factorial
  */
 
-int factorial (int n)
+int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	else if (n <= 1)
 		return (1);
 	else
-		return(n * factorial(n - 1));
+		return (n * factorial(n - 1));
 }
