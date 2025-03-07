b80f0f4cc3b3b52d136a71b1241d35529089fa1b
markjoehayes <markjoehayes@proton.me>
2025-03-07 10:15:25 -0400

    number args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print out number of cli args
+ * @argc - number of argumenyts
+ * @argv - arguments
+ * Return: 0 for sucess
+ */
+
+ int main(int argc, char __attribute__((unused)) *argv[])
+ {
+	 printf("%d\n", argc -1);
+	 return (0);
+ }
