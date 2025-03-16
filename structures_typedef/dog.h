6e7298f0c92eedcd225881b7d71f580aebe87302
markjoehayes <markjoehayes@proton.me>
2025-03-16 18:58:42 -0400

    new_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,11 +15,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-/* Additional function prototypes */
-int _putchar(char c);
+} dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
 void free_dog(dog_t *d);
 #endif
