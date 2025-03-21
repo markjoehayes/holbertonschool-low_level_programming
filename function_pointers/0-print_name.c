6979d287ae30c587876631968bc4c1f10334c429
markjoehayes <markjoehayes@proton.me>
2025-03-21 12:02:48 -0400

    added doc

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: string to print
+ * @f: functionm
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
