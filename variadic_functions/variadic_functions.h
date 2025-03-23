0f7a1bc79275a378ef70c6434a98006e214df9e6
markjoehayes <markjoehayes@proton.me>
2025-03-23 16:13:20 -0400

    print all

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,27 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct print - print
+ * @type: The operator
+ * @f: The function associated
+ */
+
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+} print_type;
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
