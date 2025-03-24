34c78dff0141e45195b37311a1e24030ad03a2ed
markjoehayes <markjoehayes@proton.me>
2025-03-24 09:01:50 -0400

    fibonacci up to 50

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - program to print the first 50 fibonacci numbers
+ * @t1: 1st number
+ * @t2: second number
+ * @nextTerm: next number in sequence
+ */
+
+int main(void)
+{
+	int i;
+	long int t1 = 0, t2 = 1;
+	long int nextTerm = t1 + t2;
+	
+	printf("%ld, %ld, ", t1, t2);
+
+	for (i=3; i <= 49; i++)
+	{
+		printf("%ld, ", nextTerm);
+		t1 = t2;
+		t2 =nextTerm;
+		nextTerm = t1 + t2;
+	}
+	printf("%ld\n", nextTerm);
+
+	return (0);
+}
--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -10,7 +10,7 @@ int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	if (c >= 'A' && c <= 'Z')
+	else if (c >= 'A' && c <= 'z')
 		return (1);
 	else
 		return (0);
